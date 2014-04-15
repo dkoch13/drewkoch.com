52b4b7e824ad2dad5950c9dd16f4854f26394bd9
Drew Koch <dkoch13@gmail.com>
2014-04-15 12:28:32 -0700

    scrolling of sections on expansion

--- js/main.js
+++ js/main.js
@@ -4,19 +4,22 @@ $("#scroll").click(function() {
 });
 
 $("#reveal-farmplicity").click(function() {
-  $("section.farmplicity").css("height", "auto");
-  $("section.farmplicity #fadeOut").hide();
+  var newHeight = $("section.farmplicity")[0].scrollHeight;
+  $("section.farmplicity").css("height", newHeight);
+  $("section.farmplicity #fadeOut").fadeOut();
   $("#reveal-farmplicity").hide();
 });
 
 $("#reveal-voyager").click(function() {
-  $("section.voyager").css("height", "auto");
+  var newHeight = $("section.voyager")[0].scrollHeight;
+  $("section.voyager").css("height", newHeight);
   $("section.voyager #fadeOut").hide();
   $("#reveal-voyager").hide();
 });
 
 $("#reveal-pantastic").click(function() {
-  $("section.pantastic").css("height", "auto");
+  var newHeight = $("section.pantastic")[0].scrollHeight;
+  $("section.pantastic").css("height", newHeight);
   $("section.pantastic #fadeOut").hide();
   $("#reveal-pantastic").hide();
 });
