c74a09b25639a62cbe92606c8d8fd22423d5c3b8
Drew Koch <dkoch13@gmail.com>
2014-04-14 23:49:54 -0700

    add teaser gradient and more content

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ $("#scroll").click(function() {
   $('html, body').animate({
     scrollTop: window.innerHeight}, 800);
 });
+
+$("#reveal").click(function() {
+  $("main section").css("height", "auto"); //hardcoded height of section for animation purposes css3
+  $("#fadeOut").hide();
+  $(this).hide();
+});
